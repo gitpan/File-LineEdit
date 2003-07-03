f42356f416e9e383e99779afad7b92c5ba917a52
Miko O'Sullivan <miko@idocs.com>
2003-07-03 21:33:00 +0000

    Import of MIKO/File-LineEdit-1.12 from CPAN.
    gitpan-cpan-distribution: File-LineEdit
    gitpan-cpan-version:      1.12
    gitpan-cpan-path:         MIKO/File-LineEdit-1.12.tar.gz
    gitpan-cpan-author:       MIKO
    gitpan-cpan-maturity:     released

--- LineEdit.pm
+++ LineEdit.pm
@@ -8,7 +8,7 @@ use overload '@{}' => sub { $_[0]->{lines} };
 
 
 # version
-$VERSION = '1.11';
+$VERSION = '1.12';
 
 
 # documentation is at the end of the file
@@ -34,19 +34,34 @@ sub new {
 	else
 		{$self->{'autosave'} = 1}
 	
+	# if lines should be automatically chomped
+	# after they are read
 	$self->{'autochomp'} = $chomp;
-	$self->{'path'} = $path;
 	
+	# the path of the original file
+	$self->{'orgpath'} = $path;
 	
+	# the path to save to, defaults to the path
+	# of the original file
+	if (exists $opts{'savepath'})
+		{$self->{'savepath'} = $opts{'savepath'}}
+	else
+		{$self->{'savepath'} = $path}
+	
+	# open a file handle for reading the original file
 	$read = FileHandle->new($path)
 		or croak "unable to read file $path: $!";
 	
+	# slurp the entire original file in
 	while (my $line = <$read>) {
 		chomp $line if $chomp;
 		push @lines, $line;
 	}
 	
+	# hold on to the lines in the original file
 	$self->{'lines'} = \@lines;
+	
+	# we're ready, return the object
 	return $self;
 }
 # 
@@ -62,8 +77,8 @@ sub save {
 	my ($self) = @_;
 	my ($out);
 	
-	$out = FileHandle->new("> $self->{'path'}")
-		or croak "unable to open file $self->{'path'} for write: $!";
+	$out = FileHandle->new("> $self->{'savepath'}")
+		or croak "unable to open file $self->{'savepath'} for write: $!";
 	
 	foreach my $line (@{$self->{'lines'}}){
 		print $out $line;
@@ -169,7 +184,8 @@ File::LineEdit can be installed with the usual routine:
 	make test
 	make install
 
-You can also just copy LineEdit.pm into the File/ directory of one of your library trees.
+You can also just copy LineEdit.pm into the File/ directory of one of your
+library trees.
 
 =head1 DESCRIPTION
 
@@ -179,10 +195,10 @@ each line (if you want it to), and then, after changes are made, writing the
 data back to the file.
 
 The basic usage is quite simple: instantiate a File::LineEdit object, loop
-through the $object->{'lines'} array, modifying each line however your want.
-When the object falls out of scope, it automatically writes the modified lines
-back to the file.  Here's a simple example (actually, the same example used
-in the synopsis above, this time with a little more documentation):
+through the object as if it were an array, modifying each line however you
+want. When the object falls out of scope, it automatically writes the modified
+lines back to the file.  Here's a simple example (actually, the same example
+used in the synopsis above, this time with a little more documentation):
 
  # instantiate a File::LineEdit object, passing in 
  # the path to the file as the only required argument.
@@ -217,6 +233,18 @@ add the C<autosave> argument to the instantiation params:
 
  my $le = File::LineEdit->new('myfile.txt', autosave=>0);
 
+=head2 Saving to a different file
+
+You might prefer to slurp data in from one file, then save to another.
+To do so, just add the C<savepath> parameter to the C<new()> call.
+For example, the following command slurps from C<source.txt> and
+saves to C<target.txt>:
+
+ my $le = File::LineEdit->new('source.txt', savepath=>'target.txt');
+
+You can also set the save path after object creation by setting
+the C<savepath> property:
+
 =head2 Automatic line chomping
 
 By default, LineEdit automatically chomps the end of each line when it slurps
@@ -228,7 +256,7 @@ add an C<autochomp> argument to the instantiation params:
 
 =head1 TIED ARRAY INTERFACE
 
-You can also use File::LineEdit the a tied array.  Just tie your
+You can also use File::LineEdit as a tied array.  Just tie your
 array to File::LineEdit::Tie, passing the file path as the 
 only argument:
 
@@ -249,7 +277,7 @@ searching and replacing.  Be sure to look at both of those modules if
 you are interested in simplified modification of files.
 
 File::LineEdit differs from File::Searcher and File::Data in that
-File::LineEdit focusses on line-based edits.  The object of
+File::LineEdit focuses on line-based edits.  The object of
 File::LineEdit is to provide a simplified manner for looking at and 
 modifying files one line at a time.
 
@@ -261,8 +289,9 @@ as Perl itself. This software comes with B<NO WARRANTY> of any kind.
 
 =head1 AUTHORS
 
-Miko O'Sullivan
-F<miko@idocs.com>
+Miko O'Sullivan F<miko@idocs.com>
+
+Useful contribution by Dan Brook.
 
 =head1 VERSION
 
@@ -276,10 +305,16 @@ Initial release
 
 Added overloading so that you can reference the LineEdit object itself 
 as if it were a reference to an array.  That was Dan Brook's idea.
-Thank Dan!
+Thanks Dan!
 
 Then I took Dan's idea a step further and added the ability to tie
 File::LineEdit to an array using File::LineEdit::Tie.
+
+=item Version 1.12    July 3, 2003
+
+Added ability to set the object to save to a different file than
+the file the data was slurped from.  Also tidied up documentation
+a little.
 
 =back
 
